cad8d304924a68cbc67b098ca2f251127a73e81c
Robin A. Dorstijn <robbiedorstijn@gmail.com>
2018-06-29 22:56:25 +0200

    Add header file

---
+++ des.h
@@ -0,0 +1,17 @@
+#ifndef ENTITY_POOL_SIZE
+#define ENTITY_POOL_SIZE 100
+#endif
+#ifndef ENTITY_COMPONENT_LIMIT
+#define ENTITY_COMPONENT_LIMIT 50
+#endif
+
+int lastGUID;
+int lastComponentID;
+
+int find_empty_row();
+int create_new_entity_row();
+int add_component_to_entity_ID(int guid, void* component);
+void add_component_to_entity_IDX (int idx, int guid, void* component);
+int remove_component_from_entity(int idx);
+void remove_entity(int guid);
+void get_entity_components(int guid, void* components[ENTITY_COMPONENT_LIMIT]);
